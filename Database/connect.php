dd3404cb4de00bc85766b9feb1b88e86e1496003
Hopedevelopers254 <design.hopedevelopers@gmail.com>
2024-10-01 16:29:28 +0300

    first commit

---
+++ Database/connect.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$db_server = 'localhost';
+$db_user ='root';
+$db_pass = '@Bashanshullam2209';
+$db_name = 'Male_Store';
+$conn = '';
+
+try{
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+}catch(mysqli_sql_exception){
+  echo'Could not connect to the database';
+}
+
+
+?>
